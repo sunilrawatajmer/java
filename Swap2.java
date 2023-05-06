13b29a2258a97a7a53dc278bb4cc52526fe2e80c
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-06 11:04:52 +0530

    Swap Two Variable

--- Swap2.java
+++ Swap2.java
@@ -3,16 +3,16 @@ class Main
 {
     public static void main (String arr[])
     {
-        int a,b,c;
+        int a,b;
         Scanner ob = new Scanner(System.in);
         System.out.println("enter two no . ");
         a = ob .nextInt();
         b = ob .nextInt();
-        c=a;
-        a=b;
-        b=c;
-        
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("swap=a="+a);
-         System.out.println("b="   +b);
+         System.out.println("b="+b);
         
     }
+}
