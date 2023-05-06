a2203ee81556180101dbada9eae333168dac8cf6
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-06 11:38:05 +0530

    String Char boolean

---
+++ string.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Demo 
+{
+    public static void main(System.in)
+    {
+        String st;
+        char ch;
+        boolien b;
+        Scanner ob =new Scanner(System.in);
+
+        System.out.print("enter string : ");
+        st = ob.nextLine();
+        System.out.println("string= "+st);
+
+        System.out.println("charecter no 0 index"+st.charAt (0));
+
+        b=true;
+        System.out.println("boolien= "+b);
+
+    }
+}
