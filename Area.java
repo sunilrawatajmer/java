677fd00725622f23622cefc87fddc6425a2a8a51
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-06 10:53:07 +0530

    rectangle Area

--- Area.java
+++ Area.java
@@ -2,6 +2,6 @@ class Area {
     public static void main (String arr[]){
         int a=10,b=20,area;
         area = a * b;
-        system.out.print("Area Of Ractangle " + arae);
+        System.out.print("Area Of Ractangle " + area);
     }
 }
