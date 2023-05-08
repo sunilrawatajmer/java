337e05dda1b8b525367cb77b6c1e67d081f22185
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:48:36 +0530

    Sum of Two Numbers

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class sum{
+    public static void main(String arr[]) {
+         int a,b,sum=0;
+
+         Scanner ob = new Scanner(System.in);
+        
+         System.out.println("Enter 1st Number ");
+         a=ob.nextInt();
+
+         System.out.println("Enter 2nd Number ");
+         b=ob.nextInt();11
+
+         sum = a + b;
+         System.out.print("Sum of Two Numbers Are "+sum);
+    }
+}
