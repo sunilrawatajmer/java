d2843e28c31095a96ba01915631a0af5a1e2362a
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:08:24 +0530

    Conditional Operator

---
+++ conditional.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class arithmatic{
+    public static void main(String a[]) {
+        int a=11,b=22,c;
+        c = a>b ? a : b ;
+        System.out.println("Smallest Number is "+c);
+    }
+}
