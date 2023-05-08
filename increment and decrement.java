b2696ebe020747126ebccd332874c7202d4d2dc0
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:08:55 +0530

    Increment and Decrement Operator

---
+++ increment and decrement.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class arithmatic{
+    public static void main(String a[]) {
+        int x=11,y=22;
+        x++ ;
+        y++ ;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+    }
+}
