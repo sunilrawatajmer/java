c4308ba18b1d0ad9930a33c2ea0da95f06f6f41a
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:39:53 +0530

    Betwise Operator

---
+++ betwise.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class betwise{
+    public static void main(String arr[]) {
+         int a;
+
+         a=4&5;
+         System.out.println(a);
+
+         a=4|5;
+         System.out.println(a);
+
+         a=4^5;
+         System.out.println(a);
+
+         a=8>>1;
+         System.out.println(a);
+
+         a=8<<1;
+         System.out.println(a);
+    }
+}
