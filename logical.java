e8216c994636d61097fb5d1e3bf7b60684ccfb34
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:21:58 +0530

    Logical Operator

---
+++ logical.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class logical{
+    public static void main(String a[]) {
+         //<<<<------LOGICAL OPERATOR------->>>>
+         System.out.println(5>2&&2<6);//AND OPERATOR
+         System.out.println(5>2&&2>6);
+         System.out.println(1>8||5<8);//OR OPERATOR
+         System.out.println(1>8||5>8);
+         //LOGICAL NOT OPERATOR
+         int u=11;
+         boolean v = (a>4);
+         System.out.println(v);
+         v = !(a>4);
+         System.out.println(v);
+    }
+}
