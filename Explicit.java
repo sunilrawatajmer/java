9c210f0cd399adec0aaa72656894d3e8806eccc6
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:35:30 +0530

    Explicit Type Casting

---
+++ Explicit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class explicit{
+    public static void main(String a[]) {
+         byte b;
+         float f=66.7f;
+         b=(byte)f;
+         System.out.println(b);
+    }
+}
