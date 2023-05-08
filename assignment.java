e532fdd3897146a07ebfdd094b163a3b9459ad3b
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:11:56 +0530

    Assignment Operator

---
+++ assignment.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class arithmatic{
+    public static void main(String a[]) {
+        int m=11,n=22;
+        m+=5;
+        n-=10;
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+    }
+}
