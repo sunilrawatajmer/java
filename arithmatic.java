a719d36b1cfccec7833b9bae0ed89c3a51d2e32c
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-08 21:04:49 +0530

    Arithmatic operator

---
+++ arithmatic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class arithmatic{
+    public static void main(String a[]) {
+        int l=2,e=10;
+        int p=l+e;
+        System.out.println("p="+p);
+        int s=l-e;
+        System.out.println("s="+s);
+    }
+}
