9faae4e3060b81fa8c13aadd4eac2d5a809e09a9
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-11 22:21:13 +0530

    Use of charAt(index)

---
+++ String3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class string{
+    public static void main(String arr[]) {
+        String a;
+        Scanner ob = new Scanner(System.in);
+
+        System.out.println("Enter a String ");
+        a=ob.next();
+        
+        System.out.println("You Entered " + a);//sunil
+        System.out.println("Char at 0 index is " + a.charAt(0));//s
+        System.out.println("Char at 3 index is " + a.charAt(3));//i
+    }
+}
+        
