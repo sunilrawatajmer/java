cb7230743f9fd7c1fb1510482c22d7e2d724a816
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-11 22:13:56 +0530

    String Example

---
+++ String2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class str{
+    public static void main(String arr[]) {
+        String a;
+        Scanner ob = new Scanner(System.in);
+
+        System.out.println("Enter a String ");
+        a=ob.next();
+        
+        System.out.println("You Entered " + a);
+    }
+}
+        
