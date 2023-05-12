7bce2761cd420598b189f30237ac17b266c3cf6f
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-12 11:41:18 +0530

    Print NUmber is Prime Number or not

--- PrimeNumberDoWhile.java
+++ PrimeNumberDoWhile.java
@@ -1,21 +1,20 @@
 import java.util.*;
 class table{
     public static void main(String arr[]) {
-        int a,x=0,i=2;
+        int a,x=0,i=1;
         Scanner ob = new Scanner(System.in);
         
         System.out.print("Enter Number ");
         a=ob.nextInt();
 
-        while(i<a){
-
-            if(a%2==0){
+        do{
+            if(a%i==0){
                 x++;
             }
             i++;
-        }
+        }while(i<=a);
 
-        if(x==0){
+        if(x==2){
             System.out.print("It is prime Number");
         }
         else
