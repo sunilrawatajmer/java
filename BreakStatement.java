48a6daf1a8f174e10b3a310914673bb0b7d3b122
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-16 09:31:31 +0530

    Break Statement

---
+++ BreakStatement.java
@@ -0,0 +1,11 @@
+class my{
+    public static void main(String ar[]) {
+        int a=1;
+        while(a<9){
+            System.out.println(a);
+            if(a>=4)
+            break;
+            a++;
+        }
+    }
+}
