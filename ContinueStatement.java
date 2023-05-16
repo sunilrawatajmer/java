5fd9ff52f214a297eed93ab4f0ac2a9563ffb962
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-16 20:38:15 +0530

    Continue Statement

--- ContinueStatement.java
+++ ContinueStatement.java
@@ -1,10 +1,10 @@
-class my{
+class dm{
     public static void main(String ar[]) {
         int a=1;
         while(a<9){
             System.out.println(a);
             if(a>=4)
-            break;
+            continue;
             a++;
         }
     }
