4046114f15fc49df260b94674cc13f80b57bcc13
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-17 10:58:21 +0530

    Maximun Number Between 4 Numbers

--- maximum4.java
+++ maximum4.java
@@ -1,63 +1,47 @@
-import java.util.*;
-class maximum{
-    public static void main(String arr[]) {
-        int num1,num2,num3,num4,max=0;
-        Scanner ob = new Scanner(System.in);
-
-        System.out.println("Enter 1st Number ");
-        num1=ob.nextInt();
-
-        System.out.println("Enter 2nd Number ");
-        num2=ob.nextInt();
-
-        System.out.println("Enter 3rd Number ");
-        num3=ob.nextInt();
-        
-        System.out.println("Enter 4th Number ");
-        num4=ob.nextInt();
-
-       
-
-        if(num1>num2){
-            if(num1>num3){
-                max=num1;
-            }
-            else{
-                max=num4;
+import java.util.Scanner;
+class Main{
+public static void main (String[] args) {
+    int num1,num2,num3,num4;
+    Scanner ob=new Scanner(System.in);
+    System.out.print("enter num1=");
+    num1=ob.nextInt();
+    System.out.print("enter num2=");
+    num2=ob.nextInt();
+    System.out.print("enter num3=");
+    num3=ob.nextInt();
+    System.out.print("enter num4=");
+    num4=ob.nextInt();
+    if(num1>num2)
+    {
+        if(num1>num3)
+        {
+            if(num1>num4)
+            {
+                System.out.println("largest="+num1);
             }
         }
-        
-        if(num2>num3){
+    }if(num2>num1)
+    {
+        if(num2>num3)
+        {
             if(num2>num4){
-                max=num2;
-            }
-            else{
-                max=num4;
+                System.out.println("largest="+num2);
             }
         }
-        
-        if(num3>num1){
-            if(num3>num4){
-                max=num3;
-            }
-            else{
-                max=num4;
-            }
-        }
-        
-        else{
-            if(num4>num1){
-                if(num4>num2){
-                    if(num4>num3){
-                        max=num4;
-                    }
-                    else{
-                        max=num2;
-                    }
-                }
+    }if(num3>num1)
+    {
+        if(num3>num2)
+        {
+            if(num3>num4)
+            {
+                System.out.println("largest="+num3);
+            }else{
+                System.out.println("largest="+num4);
             }
         }
-        
-        System.out.print("Maximum Number is ="+max);
     }
+    
+    
+    
+}
 }
