04783849fc140bbfbf7cebe74427e282439a638f
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-19 10:07:40 +0530

    Equals Method

---
+++ EqualsMethod.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Ajmer{
+public static void main(String x[]) {
+    String s1="Sunil";
+    String s2="Rawat";
+    String s3="Sunil";
+
+    System.out.println(s1.equals(s2));
+    System.out.println(s1.equals(s3));
+}
+}
