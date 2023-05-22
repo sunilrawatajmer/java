b55ecbfc08f466302f3a0a2ce8ee5cf1fe9e6410
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-22 22:09:12 +0530

    JAGGED Array Input From User

---
+++ JaggedArrayInputUser.java
@@ -0,0 +1,26 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
+public class JaggedArrayInputUser {
+    public static void main(String s[]) {
+        Scanner ob = new Scanner(System.in);
+        int a[][]=new int [3][];
+        a[0]=new int[3];
+        a[1]=new int[4];
+        a[2]=new int[2];
+
+        System.out.println("Enter Array Elements - ");
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                a[i][j]=ob.nextInt();
+            }
+        }
+
+        System.out.println("You Entered Array Elements - ");
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
