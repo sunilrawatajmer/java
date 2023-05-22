13baa9b25736ac61e8bad03609f4acdd8ed4ea48
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-22 21:34:14 +0530

    Using Function

---
+++ UsingFunction.java
@@ -0,0 +1,16 @@
+public class UsingFunction {
+    public static void main(String a[]){
+        System.out.println("This is Main Function ");
+        fun1();
+        fun2();
+        System.out.println("This is Main Function Again");
+    } 
+
+    public static void fun1(){
+        System.out.println("This is Function 1");
+    }
+
+    public static void fun2(){
+        System.out.println("This is Function 2");
+    }
+}
