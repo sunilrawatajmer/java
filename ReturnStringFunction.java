7e8e92963dff979871dd5015882d1dd491dda2e5
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-23 21:07:25 +0530

    Return & Print Maximum  String Between Two Strings

--- ReturnStringFunction.java
+++ ReturnStringFunction.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 public class ReturnStringFunction {
     public static void main(String[] args) {
         Scanner ob = new Scanner (System.in);
-        System.out.println("Enter Two String : ");
+        System.out.println("Enter First String : ");
         String a=ob.next();
+        System.out.println("Enter Second String : ");
         String b=ob.next();
         String  k = tmp(a,b);
-        System.out.println("largest ="+k);
+        System.out.println("Result ="+k);
     }
 
     static String tmp(String x,String y){
-      String c=x;
-      String d=y;
        int len1= x.length();
         int len2= y.length();
         if(len1>len2)
-        return c;
-        
+        return x;
+        else if(len2>len1)
+        return y;
         else
-        return d;
+        return "Both of Equal";
     }
 }
