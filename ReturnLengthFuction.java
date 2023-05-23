fb354421cb53dc60ef276664e27b5ba46f506fb0
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-05-23 20:49:47 +0530

    Return String Length Using Function

---
+++ ReturnLengthFuction.java
@@ -0,0 +1,10 @@
+public class ReturnLengthFuction {
+    public static void main(String[] args) {
+        int k = tmp("sunil");
+        System.out.println("Length of String = "+k);
+    }
+
+    static int tmp(String st){
+        return st.length();
+    }
+}
