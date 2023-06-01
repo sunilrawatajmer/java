7e2a5cc1f08272b9b3681129cf7ef042f127ffc2
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-06-01 20:31:32 +0530

    Example of HashSet Class

---
+++ HashSet1.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+public class HashSet1 {
+   public static void main(String[] args) {
+       HashSet s1 = new HashSet();
+
+       s1.add("Volvo");
+       s1.add("BMW");
+       s1.add("Ford");
+       s1.add("BMW");
+       s1.add("Mazda");
+       //s1.equals("sunil");
+       System.out.println(s1.);
+
+   }
+}
