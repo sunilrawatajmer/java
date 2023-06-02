2031f765eebef0a792629f56059124fea4a53ada
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-06-02 23:21:17 +0530

    Map Properties

---
+++ MapProperties.java
@@ -0,0 +1,21 @@
+/*
+-----------MAP Properties-----------
+
+1) package  : java.util
+2) Map Stores The Data in Key-Value pairs
+3) For key Value we use Unique Value
+
+ Exa.--         KEY     VALUE
+               101     Sunil
+               102     Anil
+               103     Rawat
+           
+ 4){Key Must Be Unique Also But Value Can be Duplicate }
+ 5) {key + value = its Called - Entry}
+ 6) Map Does Not Follow Sorting And Insertion Order
+ 7) We can Insert Only One NULL value in Key But 
+    In VAlue We Can INsert Multipal NULL Values
+
+
+*/
+
