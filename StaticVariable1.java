d5ef75468cbf76901b7c0e99bf62beba7658aebd
sunilrawatajmer <sunilsoftdev50@gmail.com>
2023-06-25 22:42:01 +0530

    Static Variable Use

--- StaticVariable1.java
+++ StaticVariable1.java
@@ -2,18 +2,20 @@
  This is Common Variable of Class Objects 
  This is Not Uniique For All Objects 
  Ints Take Space Once Time in class
- 
+
 */
 
 class Demo1{
     static int a;
+    int b;
 
-    void SetA(int x){
+    void SetA(int x,int y){
         a=x;
+        b=y;
     }
 
     void show (){
-        System.out.println("a = "+a);
+        System.out.println("a = "+a+ " b = "+b);
     }
 
 }
@@ -23,8 +25,8 @@ class StaticVariable{
         Demo1 ob1 = new Demo1();
         Demo1 ob2 = new Demo1();
 
-        ob1.SetA(10);
-        ob1.SetA(20);
+        ob1.SetA(10,50);
+        ob2.SetA(20,100);
 
         ob1.show();
         ob2.show();
